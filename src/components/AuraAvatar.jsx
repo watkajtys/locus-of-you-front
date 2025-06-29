c1942d0d1d2093120e8e1b918988544c04e6f85f
Matt Watkajtys <watkajtys@gmail.com>
2025-06-28 21:18:25 -0600

    Make aura a full circle

--- src/components/AuraAvatar.jsx
+++ src/components/AuraAvatar.jsx
@@ -62,16 +62,6 @@ const AuraAvatar = ({ size = 128, className = '' }) => {
       role="img"
       aria-label={`Aura avatar in ${auraState} state`}
     >
-      {/* Optional: Add content inside the circle */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div 
-          className="w-1/3 h-1/3 rounded-full opacity-60"
-          style={{ 
-            backgroundColor: 'var(--color-background)',
-            backdropFilter: 'blur(8px)'
-          }}
-        />
-      </div>
     </div>
   );
 };
